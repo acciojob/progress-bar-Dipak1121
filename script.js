09abf5c811b7fc204983dc730aa2475598f3863c
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-07 22:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+
+next.addEventListener("click", onNextClick);
+prev.addEventListener("click", onPrevClick);
+
+let ind = 1;
+function onNextClick() {
+	if(ind >= 5){
+		
+		return;
+	}
+	let line = document.getElementById(`line${ind}`);
+	line.classList.toggle("active");
+	ind++;
+	let circle = document.getElementById(`circle${ind}`);
+	circle.classList.toggle("active");
+}
+
+function onPrevClick() {
+	
+}
