d1adb0e034ecca1daaa6448a18b1b3248507f354
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-07 23:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ let prev = document.getElementById("prev");
 
 next.addEventListener("click", onNextClick);
 prev.addEventListener("click", onPrevClick);
-
+prev.disabled = true;
+prev.classList.add("disable");
 let ind = 1;
-document.getElementById(`circle${ind}`).classList.add("active");
+document.getElementById(`circle-${ind}`).classList.add("active");
 function onNextClick() {
 	if(ind >= 5){
 		return;
 	}
 	else if(prev.classList.contains("disable")){
 		prev.classList.remove("disable");
+		prev.disabled = false;
 	}
 	let line = document.getElementById(`line${ind}`);
 	line.classList.toggle("active");
 	ind++;
-	let circle = document.getElementById(`circle${ind}`);
+	let circle = document.getElementById(`circle-${ind}`);
 	circle.classList.toggle("active");
 	if(ind == 5){
 		next.classList.add("disable");
+		next.disabled = true;
 	}
 }
 
@@ -29,14 +32,15 @@ function onPrevClick() {
 	}
 	else if(next.classList.contains("disable")){
 		next.classList.remove("disable");
-		
+		next.disabled = false;
 	}
-	let circle = document.getElementById(`circle${ind}`);
+	let circle = document.getElementById(`circle-${ind}`);
 	circle.classList.toggle("active");
 	ind--;
 	let line = document.getElementById(`line${ind}`);
 	line.classList.toggle("active");
 	if ( ind == 1 ){
 		prev.classList.add("disable");
+		prev.disabled = true;
 	}
 }
